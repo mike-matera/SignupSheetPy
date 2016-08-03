97786d83dfe86f2d8b85a322551ca853e574d512
Mike Matera <matera@lifealgorithmic.com>
2016-08-03 09:02:04 -0700

    Add email addresses to the signups sheet.

--- signup/views.py
+++ signup/views.py
@@ -290,7 +290,7 @@ def download_csv(request):
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="StaffSheet.csv"'
     writer = csv.writer(response)
-    writer.writerow(["Role", "Protected", "EA/LD", "Job", "Start Day", "Start Time", "End Day", "End Time", "Person"])
+    writer.writerow(["Role", "Protected", "EA/LD", "Job", "Start Day", "Start Time", "End Day", "End Time", "Name", "Email"])
     total = 0
     taken = 0;
     
@@ -308,12 +308,12 @@ def download_csv(request):
         if is_ld(j) :
             eald = "Late Departure"
 
-        for v in Volunteer.objects.filter(source__exact=j.source.pk, title__exact=j.title, start__exact=j.start) :                
-            writer.writerow([j.source.pk, prot, eald, j.title, j.start.strftime(DAYFORMAT), j.start.strftime(TIMEFORMAT), j.end.strftime(DAYFORMAT), j.end.strftime(TIMEFORMAT), v.name])
+        for v in Volunteer.objects.filter(source__exact=j.source.pk, title__exact=j.title, start__exact=j.start).select_related('user') :
+            writer.writerow([j.source.pk, prot, eald, j.title, j.start.strftime(DAYFORMAT), j.start.strftime(TIMEFORMAT), j.end.strftime(DAYFORMAT), j.end.strftime(TIMEFORMAT), v.name, v.user.email])
             cnt += 1
             
         for _ in xrange(0, j.needs - cnt) :
-            writer.writerow([j.source.pk, prot, eald, j.title, j.start.strftime(DAYFORMAT), j.start.strftime(TIMEFORMAT), j.end.strftime(DAYFORMAT), j.end.strftime(TIMEFORMAT), ""])
+            writer.writerow([j.source.pk, prot, eald, j.title, j.start.strftime(DAYFORMAT), j.start.strftime(TIMEFORMAT), j.end.strftime(DAYFORMAT), j.end.strftime(TIMEFORMAT), "", ""])
 
         total += j.needs
         taken += cnt
