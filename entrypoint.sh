7f08edbb34bdb281eb8039aa529e0cddb8217623
Mike Matera <matera@lifealgorithmic.com>
2022-02-15 21:09:33 -0800

    deploys to minikube

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ python3 ./manage.py migrate
 if [ ! -f first-start ]; then 
     echo Running first-start tasks.
     echo username: $DJANGO_ADMIN_USERNAME
-    echo email: $DJANGO_ADMIN_USERNAME
+    echo email: $DJANGO_ADMIN_EMAIL
     echo password: $DJANGO_SUPERUSER_PASSWORD
     python3 ./manage.py createsuperuser --no-input --email $DJANGO_ADMIN_EMAIL --username $DJANGO_ADMIN_USERNAME
     touch first-start
