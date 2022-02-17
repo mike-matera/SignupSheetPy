7a895f10ffc7d317d85cfca38e75fe86c4448b3b
Mike Matera <maximus@fatboycentral.com>
2022-02-16 19:08:15 -0800

    Fix entrypoint.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,5 @@
 set -e 
 
 python3 ./manage.py migrate 
-
-if [ ! -f first-start ]; then 
-    echo Running first-start tasks.
-    echo username: $DJANGO_ADMIN_USERNAME
-    echo email: $DJANGO_ADMIN_EMAIL
-    echo password: $DJANGO_SUPERUSER_PASSWORD
-    python3 ./manage.py createsuperuser --no-input --email $DJANGO_ADMIN_EMAIL --username $DJANGO_ADMIN_USERNAME
-    touch first-start
-fi 
-
+python3 ./manage.py createsuperuser --no-input --email $DJANGO_ADMIN_EMAIL --username $DJANGO_ADMIN_USERNAME || true
 python3 ./manage.py runserver 0.0.0.0:8000
