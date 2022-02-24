7ae8efd319036801bedd0061864c2b4c2c28a4d8
Mike Matera <matera@lifealgorithmic.com>
2022-02-23 22:11:50 -0800

    Add react.

--- fnf/settings.py
+++ fnf/settings.py
@@ -77,22 +77,24 @@ WSGI_APPLICATION = 'fnf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
+DATABASES_CHOICES = {
     'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
     'mariadb': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['DJANGO_DB_NAME'],
-        'USER': os.environ['DJANGO_DB_USER'],
-        'PASSWORD': os.environ['DJANGO_DB_PASSWORD'],
-        'HOST': os.environ['DJANGO_DB_HOST'],
-        'PORT': os.environ['DJANGO_DB_PORT'],
+        'NAME': os.environ.get('DJANGO_DB_NAME'),
+        'USER': os.environ.get('DJANGO_DB_USER'),
+        'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD'),
+        'HOST': os.environ.get('DJANGO_DB_HOST'),
+        'PORT': os.environ.get('DJANGO_DB_PORT'),
     },
 }
 
-DATABASES['default'] = DATABASES[os.environ["DJANGO_DB"]]
+DATABASES = {
+    'default': DATABASES_CHOICES[os.environ.get("DJANGO_DB", 'sqlite')],
+}
 
 
 # Password validation
