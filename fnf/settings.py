416eb0875cfe693a0bd270a4329ccc178d260695
Mike Matera <maximus@fatboycentral.com>
2022-03-09 09:53:34 -0800

    Add the ability to send mail. Get SendGrid working.

--- fnf/settings.py
+++ fnf/settings.py
@@ -160,3 +160,15 @@ CACHES = {
 }
 
 LOGIN_REDIRECT_URL = "/"
+
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST=os.environ['EMAIL_HOST']
+    EMAIL_PORT=os.environ['EMAIL_PORT']
+    EMAIL_HOST_USER=os.environ['EMAIL_HOST_USER']
+    EMAIL_HOST_PASSWORD=os.environ['EMAIL_HOST_PASSWORD']
+    DEFAULT_FROM_EMAIL=os.environ['DEFAULT_FROM_EMAIL']
+    EMAIL_USE_TLS=True
+    
